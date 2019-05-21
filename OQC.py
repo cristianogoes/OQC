78e76b7a7d7f2d8807e59c80a0211e83df00026e
cristianogoes <cristiano.cgoes@gmail.com>
2019-05-21 08:00:22 -0400

    Limpeza no código

--- OQC.py
+++ OQC.py
@@ -1,18 +1,7 @@
 
 import time
 import pandas as pd
-from collections import deque
 import Comunicacao, ScannerFile
-
-list_name = []
-list_status = []
-list_port = []
-list_station = []
-
-deq_name = deque(list_name)
-deq_status = deque(list_status)
-deq_port = deque(list_port)
-deq_station = deque(list_station)
 
 df0=[]
 DFX0 = pd.DataFrame(df0)
@@ -48,11 +37,6 @@ if __name__=='__main__':
                 station = scanner.readFile(newFile[ii],7)[1:2]
                 porta = newFile[ii][(len(newFile[ii])-5):(len(newFile[ii])-4)]
 
-                deq_port.append(porta)
-                deq_station.append(station)
-                deq_name.append(newFile[ii])
-                deq_status.append(status_indice)
-
                 dfArrayjig = dfArrayjig.append(pd.Series([station, porta, status_indice], index=dfArrayjig.columns), ignore_index=True)
 
                 ii += 1
@@ -71,7 +55,7 @@ if __name__=='__main__':
             DFX7 = DFX7.reset_index(drop=True)
             dfArrayjig.drop(dfArrayjig[dfArrayjig.station == '7'].index, inplace=True)
 
-        frames = [DFX0,DFX6, DFX7]
+        frames = [DFX0, DFX6, DFX7]
         X1 = pd.concat(frames)
         X1 = X1.reset_index(drop=True)
         print('Database para envio')
@@ -95,34 +79,3 @@ if __name__=='__main__':
                 DFX6 = DFX6.drop([0, 1, 2, 3], axis=0)
             if DFX7.size:
                 DFX7= DFX7.drop([0, 1, 2, 3], axis=0)
-"""
-        if deq_status:
-            print("TAMANHO: ",len(deq_status))
-            print("Pass: ",deq_status.count("P\n"))
-            print("Fail: ",deq_status.count("F\n"))
-
-            #Fila - FIFO
-            a = deq_station.popleft()
-            b = deq_port.popleft()
-            deq_name.popleft()
-            value = deq_status.popleft()
-            wwww = a+b+value
-            print("> :",wwww)
-
-            #Pilha - LIFO
-            #deq_station.pop()
-            #deq_port.pop()
-            #deq_name.pop()
-            #value = deq_status.pop()
-            
-            seri.serialWrite(wwww)
-
-            out = t.readline()
-
-            if out != '':
-                if out == b'finalizado\n':
-                    print(">> NEXT: ")
-                out = ''
-        else:
-            print("Fila vazia: ")
-"""
